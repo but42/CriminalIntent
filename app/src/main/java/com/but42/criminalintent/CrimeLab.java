70309ca385aef9e542323eaac31c7cc6f8066e3f
Ted <but42@bk.ru>
2016-11-05 21:30:58 +0300

    Add database

--- app/src/main/java/com/but42/criminalintent/CrimeLab.java
+++ app/src/main/java/com/but42/criminalintent/CrimeLab.java
@@ -1,6 +1,13 @@
 package com.but42.criminalintent;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import com.but42.criminalintent.database.CrimeBaseHelper;
+import com.but42.criminalintent.database.CrimeCursorWrapper;
+import com.but42.criminalintent.database.CrimeDbSchema;
+import com.but42.criminalintent.database.CrimeDbSchema.CrimeTable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,10 +17,9 @@ import java.util.UUID;
  * Created by mikhail on 17/10/16.
  */
 public class CrimeLab {
-    private List<Crime> mCrimes;
-
     private static CrimeLab sCrimeLab;
-    private Context mAppContext;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context c) {
         if (sCrimeLab == null) {
@@ -22,28 +28,75 @@ public class CrimeLab {
         return sCrimeLab;
     }
 
-    private CrimeLab(Context appContext) {
-        this.mAppContext = appContext;
-        mCrimes = new ArrayList<>();
+    private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
     }
 
     public List<Crime> getCrimes() {
-        return mCrimes;
+        List<Crime> crimes = new ArrayList<>();
+        CrimeCursorWrapper cursor = queryCrimes(null, null);
+        try {
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                crimes.add(cursor.getCrime());
+                cursor.moveToNext();
+            }
+        } finally {
+            cursor.close();
+        }
+        return crimes;
     }
 
     public Crime getCrime(UUID id) {
-        for (Crime c : mCrimes) {
-            if (c.getId().equals(id))
-                return c;
+        CrimeCursorWrapper cursor = queryCrimes(CrimeTable.Cols.UUID + " = ?", new String[] { id.toString() });
+        try {
+            if (cursor.getCount() == 0) {
+                return null;
+            }
+
+            cursor.moveToFirst();
+            return cursor.getCrime();
+        } finally {
+            cursor.close();
         }
-        return null;
     }
 
     public void addCrime(Crime c) {
-        mCrimes.add(c);
+        ContentValues values = getContentValues(c);
+        mDatabase.insert(CrimeTable.NAME, null, values);
     }
 
     public void deleteCrime(Crime crime) {
-        mCrimes.remove(crime);
+        mDatabase.delete(CrimeTable.NAME, CrimeTable.Cols.UUID + " = ?", new String[] { crime.getId().toString() });
+    }
+
+    public void updateCrime(Crime crime) {
+        String uuidString = crime.getId().toString();
+        ContentValues values = getContentValues(crime);
+
+        mDatabase.update(CrimeTable.NAME, values, CrimeTable.Cols.UUID + " = ?", new String[] { uuidString });
+    }
+
+    private static ContentValues getContentValues(Crime crime) {
+        ContentValues values = new ContentValues();
+        values.put(CrimeTable.Cols.UUID, crime.getId().toString());
+        values.put(CrimeTable.Cols.TITLE, crime.getTitle());
+        values.put(CrimeTable.Cols.DATE, crime.getDate().getTime());
+        values.put(CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
+        return values;
+    }
+
+    private CrimeCursorWrapper queryCrimes(String whereClaus, String[] whereArgs) {
+        Cursor cursor = mDatabase.query(
+                CrimeTable.NAME,
+                null,
+                whereClaus,
+                whereArgs,
+                null,
+                null,
+                null
+        );
+        return new CrimeCursorWrapper(cursor);
     }
 }
---
+++ app/src/main/java/com/but42/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,17 @@
+package com.but42.criminalintent.database;
+
+/**
+ * Created by but on 05.11.2016.
+ */
+public class CrimeDbSchema {
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
